974a7f8c3d8ce55d5d880e4641e7ac1b59f96519
Michelle Schulp <michelle@marktimemedia.com>
2021-05-20 11:21:08 -0500

    better UI for true/false selection

--- templates/mtm-content-grid-view.php
+++ templates/mtm-content-grid-view.php
@@ -3,14 +3,19 @@ global $mtm_grid_row_class;
 ?>
 <article class="mtm-grid--single <?php echo esc_attr( $mtm_grid_row_class ); ?>" id="<?php echo esc_attr( mtm_anchor( get_the_title() ) ); ?>">
 	<div class="mtm-grid--single-content">
-		<?php if ( has_post_thumbnail() ) : ?>
+		<?php if ( has_post_thumbnail() && get_field( 'mtm_show_image' ) ) : ?>
 			<section class="mtm-grid--image">
 				<a aria-hidden="true" tabindex="-1" href="<?php the_permalink(); ?>"><figure class="post--thumbnail mtm-post-thumbnail has-background-image cropped" style="background-image:url(<?php the_post_thumbnail_url( 'medium_large' ); ?>)"></figure></a>
 			</section>
 		<?php endif; ?>
 		<section class="mtm-grid--post-content">
 			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-			<?php the_excerpt(); ?>
+			<?php if ( get_field( 'mtm_show_date' ) ) : ?>
+				<p class="post--byline"><?php the_time( 'F j, Y' ); ?></p>
+			<?php endif; ?>
+			<?php if ( get_field( 'mtm_show_excerpt' ) ) :
+				the_excerpt();
+			endif; ?>
 		</section>
 	</div>
 </article>
--- templates/mtm-content-list-view.php
+++ templates/mtm-content-list-view.php
@@ -1,13 +1,17 @@
 <article class="mtm-list--single">
-	<?php $content_size = has_post_thumbnail() ? '' : '-full'; ?>
-	<?php if ( has_post_thumbnail() ) : ?>
+	<?php $content_size = has_post_thumbnail() && get_field( 'mtm_show_image' ) ? '' : '-full'; ?>
+	<?php if ( has_post_thumbnail() && get_field( 'mtm_show_image' ) ) : ?>
 		<section class="mtm-list--image">
 			<a aria-hidden="true" tabindex="-1" href="<?php the_permalink(); ?>"><figure class="post--thumbnail mtm-post-thumbnail has-background-image cropped" style="background-image:url(<?php the_post_thumbnail_url( 'medium_large' ); ?>)"></figure></a>
 		</section>
 	<?php endif; ?>
 	<section class="mtm-list--post-content<?php echo esc_attr( $content_size ); ?>">
 		<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-		<p class="post--byline"><?php the_time( 'F j, Y' ); ?></p>
-		<?php the_excerpt(); ?>
+		<?php if ( get_field( 'mtm_show_date' ) ) : ?>
+			<p class="post--byline"><?php the_time( 'F j, Y' ); ?></p>
+		<?php endif; ?>
+		<?php if ( get_field( 'mtm_show_excerpt' ) ) : ?>
+			<?php the_excerpt(); ?>
+		<?php endif; ?>
 	</section>
 </article>
